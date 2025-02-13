0eaa09b64dbe69ddb9fcd1486da1912a933da2c5
Divya-Verma22 <dv7991393@gmail.com>
2025-02-13 23:28:13 +0530

    Add files via upload
    OddOrEven

---
+++ oddeven.java
@@ -0,0 +1,14 @@
+package bitwiseoperator;
+
+public class oddeven {
+    public static void main(String[] args) {
+        int n = 78;
+        System.out.println(isodd(n));
+
+
+    }
+    public static boolean isodd(int n){
+        return (n&1)==1;
+    }
+
+}
